31acdf967783dd210c88360909a79b8796b268ef
imranpollob <polboy777@gmail.com>
2022-01-25 17:29:36 -0500

    remove last node from linked list

--- geeksforgeeks/linkedlist/SinglyLinkedList/src/SinglyLinkedList.java
+++ geeksforgeeks/linkedlist/SinglyLinkedList/src/SinglyLinkedList.java
@@ -1,13 +1,12 @@
 class Node {
 	int value;
 	Node next;
-	
+
 	Node(int value) {
 		this.value = value;
 		this.next = null;
 	}
 }
-
 
 public class SinglyLinkedList {
 
@@ -21,7 +20,7 @@ public class SinglyLinkedList {
 		head = addFirst(head, 50);
 		head = addFirst(head, 60);
 		head = addFirst(head, 70);
-		
+
 		Node new_head = null;
 		new_head = addFirst(new_head, 1000);
 		new_head = addFirst(new_head, 2000);
@@ -30,14 +29,14 @@ public class SinglyLinkedList {
 		new_head = addLast(new_head, 3000);
 		new_head = addLast(new_head, 4000);
 		listNodes(new_head);
-		
+
 		Node new_head2 = null;
+		new_head2 = addLast(new_head2, 70);
 		new_head2 = addLast(new_head2, 80);
 		new_head2 = addLast(new_head2, 90);
 		new_head2 = addLast(new_head2, 100);
 		System.out.println("List");
 		listNodesRecursively(new_head2);
-		
 
 		System.out.println("Removing from the first");
 		new_head2 = removeFirst(new_head2);
@@ -45,26 +44,36 @@ public class SinglyLinkedList {
 		System.out.println("Removing from the first");
 		new_head2 = removeFirst(new_head2);
 		listNodes(new_head2);
-//		new_head3 = removeLast(new_head3);
-//		new_head3 = removeLast(new_head3);
-//		new_head3 = removeLast(new_head3);
+		System.out.println("Removing from the last");
+		new_head2 = removeLast(new_head2);
+		listNodes(new_head2);
+		System.out.println("Removing from the last");
+		new_head2 = removeLast(new_head2);
+		listNodes(new_head2);
 	}
 
 	private static Node removeFirst(Node head) {
 		if (head == null) {
 			return null;
 		}
-		
+
 		return head.next;
 	}
 
 	private static Node removeLast(Node head) {
-		Node current = head;
-		
-		while (current.next != null) {
-			
+		if (head == null || head.next == null) {
+			return null;
 		}
-		return null;
+
+		Node current = head;
+
+		while (current.next.next != null) {
+			current = current.next;
+		}
+
+		current.next = null;
+
+		return head;
 	}
 
 	private static void listNodes(Node head) {
@@ -77,7 +86,7 @@ public class SinglyLinkedList {
 	private static void listNodesRecursively(Node head) {
 		if (head == null)
 			return;
-		
+
 		System.out.println(head.value);
 		listNodesRecursively(head.next);
 	}
@@ -87,26 +96,23 @@ public class SinglyLinkedList {
 		new_head.next = head;
 		return new_head;
 	}
-	
+
 	private static Node addLast(Node head, int value) {
 		Node temp = new Node(value);
-		
+
 		if (head == null) {
 			return temp;
 		}
-		
+
 		Node current = head;
-		
+
 		while (current.next != null) {
 			current = current.next;
 		}
-		
+
 		current.next = temp;
-		
+
 		return head;
 	}
-
-	
-
 
 }
